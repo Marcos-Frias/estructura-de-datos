ee0ceda16520c59abe0a60dcc2ded4d27beb5d1d
Marcos-Frias <mark.frias.mtz@gmail.com>
2020-04-22 08:30:09 -0500

    Array

--- src/estructuradatos/EstructuraDatos.java
+++ src/estructuradatos/EstructuraDatos.java
@@ -15,7 +15,26 @@ public class EstructuraDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        /* Array */
+        
+        /*Declaración eh inicialización de un array de int*/
+        int[] array_number;
+        array_number = new int[5];
+        
+        /*Agregar valores a un array*/
+        array_number[0] = 100;
+        array_number[1] = 200;
+        array_number[2] = 300;
+        array_number[3] = 400;
+        array_number[4] = 500;
+        
+        /*Recorrer un array*/
+        for(int i = 0; i < array_number.length ; i++){
+            /*Mostrar en pantalla el array*/
+            System.out.println(array_number[i]);
+        } 
+        
     }
     
 }
