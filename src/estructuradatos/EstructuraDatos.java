5d637b4806099271515f4804541d11ebf3b1052e
Marcos-Frias <mark.frias.mtz@gmail.com>
2020-04-22 08:31:28 -0500

    Matriz

--- src/estructuradatos/EstructuraDatos.java
+++ src/estructuradatos/EstructuraDatos.java
@@ -19,21 +19,49 @@ public class EstructuraDatos {
         /* Array */
         
         /*Declaración eh inicialización de un array de int*/
-        int[] array_number;
-        array_number = new int[5];
+//        int[] array_number;
+//        array_number = new int[5];
         
         /*Agregar valores a un array*/
-        array_number[0] = 100;
-        array_number[1] = 200;
-        array_number[2] = 300;
-        array_number[3] = 400;
-        array_number[4] = 500;
+//        array_number[0] = 100;
+//        array_number[1] = 200;
+//        array_number[2] = 300;
+//        array_number[3] = 400;
+//        array_number[4] = 500;
         
         /*Recorrer un array*/
-        for(int i = 0; i < array_number.length ; i++){
-            /*Mostrar en pantalla el array*/
-            System.out.println(array_number[i]);
-        } 
+//        for(int i = 0; i < array_number.length ; i++){
+//            /*Mostrar en pantalla el array*/
+//            System.out.println(array_number[i]);
+//        } 
+        
+        /* Matrices */
+        
+        /*Declaración eh inicialización de una matriz de int*/
+        int[][] matriz_number;
+        matriz_number = new int[3][3];
+        
+        /*Agregar valores a una matriz*/
+        matriz_number[0][0] = 100;
+        matriz_number[0][1] = 100;
+        matriz_number[0][2] = 100;
+        matriz_number[1][0] = 200;
+        matriz_number[1][1] = 200;
+        matriz_number[1][2] = 200;
+        matriz_number[2][0] = 300;
+        matriz_number[2][1] = 300;
+        matriz_number[2][2] = 300;
+        
+        /*Matriz inicializada con valores*/
+        //matriz_number = {  {10,20,30},{40,50,60},{70,80,90},  {10,20,30},{40,50,60},{70,80,90} };
+        
+        /*Recorrer una matriz*/
+        for (int i = 0; i < matriz_number.length ; i++) {
+            for (int j = 0; j < matriz_number[i].length; j++) {
+                System.out.print(matriz_number[i][j]+" | ");
+            }
+            System.out.println();
+        }
         
     }
     
