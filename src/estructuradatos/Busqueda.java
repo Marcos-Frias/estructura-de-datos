477baec8167440976c98238ff0b65e868c4b225e
Marcos-Frias <mark.frias.mtz@gmail.com>
2020-05-04 18:15:03 -0500

    Clase de busqueda

---
+++ src/estructuradatos/Busqueda.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuradatos;
+
+/**
+ *
+ * @author marcos
+ */
+public class Busqueda {
+    
+    public int searchBinary(int val, int[] data){
+        
+        return -1;
+    }
+    
+}
